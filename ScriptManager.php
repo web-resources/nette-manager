65a11408c271a42876ffddaecdcb3821856a845a
Michal Gebauer <mishak@mishak.net>
2012-11-16 16:27:20 +0100

    Init

---
+++ ScriptManager.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Mishak\WebResourceManagement;
+
+use Nette,
+	Nette\Utils\Html;
+
+class ScriptManager {
+
+	/**
+	 * Definition of scripts
+	 *
+	 * @var array
+	 */
+	private $scripts = [];
+
+	private $translator;
+
+	public function __construct($scripts, Nette\Localization\ITranslator $translator)
+	{
+		$this->scripts = $scripts;
+		$this->translator = $translator;
+	}
+
+	private $public = TRUE;
+
+	public function usePublic($use)
+	{
+		$this->public = $use;
+		return $this;
+	}
+
+	public function setRequired($scripts)
+	{
+		$this->required = [];
+		$this->dependencies = [];
+		$this->queue = [];
+		foreach ($scripts as $script) {
+			$this->add($script);
+		}
+		return $this;
+	}
+
+	private $path;
+
+	/**
+	 * Set server path to scripts
+	 *
+	 * @param string $path
+	 */
+	public function setPath($path)
+	{
+		$this->path = $path;
+		return $this;
+	}
+
+	private $presenter;
+
+	/**
+	 * Set presenter (is passed to script config)
+	 *
+	 * @param string $presenter
+	 */
+	public function setPresenter($presenter)
+	{
+		$this->presenter = $presenter;
+		return $this;
+	}
+
+	public function output()
+	{
+		$fragment = Html::el();
+		while ($this->queue) {
+			$printed = FALSE;
+			$scripts = $this->queue;
+			$this->queue = [];
+			foreach ($scripts as $script) {
+				$fragment[] = $this->outputScript($script);
+				$fragment[] = "\n";
+				$this->addScriptDependenciesToQueue($script);
+			}
+		}
+		return $fragment;
+	}
+
+	private $minified = TRUE;
+
+	/**
+	 * All required scripts
+	 *
+	 * @var array
+	 */
+	private $required;
+
+	/**
+	 * Map of script dependencies
+	 *
+	 * @var array[$scriptName][$dependantName] = $dependant
+	 */
+	private $dependencies;
+
+	/**
+	 * Queue of scripts to print
+	 *
+	 * @var array
+	 */
+	private $queue;
+
+	/**
+	 * Adds script identified by name to required scripts
+	 *
+	 * @param string $name
+	 * @return object
+	 */
+	private function add($name)
+	{
+		if (isset($this->required[$name])) {
+			return $this->required[$name];
+		}
+		if (!isset($this->scripts[$name])) {
+			throw new \Exception("Script '$name' has no definition.");
+		}
+		$script = (object) $this->scripts[$name];
+		$script->name = $name;
+		$script->printed = FALSE;
+		$script->depends = isset($script->depends) ? (is_array($script->depends) ? $script->depends : [ $script->depends ]) : [];
+		foreach ($script->depends as $dependency) {
+			$this->add($dependency);
+			$this->dependencies[$dependency][$script->name] = $script;
+		}
+		if (!$script->depends) {
+			$this->queue[] = $script;
+		}
+		return $this->required[$name] = $script;
+	}
+
+	/**
+	 * filename - dev
+	 * minified - prod
+	 * public - prod
+	 */
+	private function outputScript($script)
+	{
+		$productionMode = $this->presenter->context->parameters['productionMode'];
+		if ($productionMode && $this->public && isset($script->public)) {
+			$filename = $script->public;
+		} elseif (isset($script->minified) && ($productionMode || !isset($script->filename))) {
+			$filename = $script->minified;
+		} elseif (isset($script->filename)) {
+			$filename = $script->filename;
+		} else {
+			throw new \Exception("Script '$script->name' is missing filename or its minified version.");
+		}
+		$fragment = Html::el();
+		if (isset($script->translations)) {
+			$init = 'var translations = typeof translations == \'undefined\' ? {} : translations;';
+			$content = [ $init ];
+			foreach ($script->translations as $text) {
+				$content[] = 'translations[' . json_encode($text) . '] = ' . json_encode($this->translator ? $this->translator->translate($text) : $text) . ';';
+			}
+			$fragment->create('script', [ 'type' => 'text/javascript'])->setText("\n" . implode("\n", $content));
+		}
+		if (!empty($script->include)) {
+			$fragment->create('script', [ 'type' => 'text/javascript' ])->setText(file_get_contents(WWW_DIR . '/js/' . $filename));
+		} else {
+			$fragment->create('script', [
+				'src' => parse_url($filename, PHP_URL_SCHEME) || substr($filename, 0, 2) === '//' ? $filename : $this->path . '/' . $filename,
+				'type' => 'text/javascript'
+			]);
+		}
+		if (isset($script->config)) {
+			$class = $script->config['class'];
+			$config = new $class;
+			$variables = $config->getVariables($script->name, $this->presenter);
+			$content = [];
+			foreach ($variables as $name => $value) {
+				$line = '';
+				if (FALSE === strpos($name, '.')) {
+					$line = 'var ';
+				}
+				$line .= $name . ' = ' . json_encode($value) . ";\n";
+				$content[] = $line;
+			}
+			$fragment[] = "\n";
+			$fragment->create('script', [ 'type' => 'text/javascript' ])->setText("\n" . implode("\n", $content));
+		}
+		$script->printed = TRUE;
+		return $fragment;
+	}
+
+	private function addScriptDependenciesToQueue($script)
+	{
+		if (isset($this->dependencies[$script->name])) {
+			foreach ($this->dependencies[$script->name] as $dependant) {
+				if ($dependant->printed) {
+					continue;
+				}
+				$pendingDependencies = FALSE;
+				foreach ($dependant->depends as $name) {
+					if (!$this->required[$name]->printed) {
+						$pendingDependencies = TRUE;
+						break;
+					}
+				}
+				if (!$pendingDependencies) {
+					$this->queue[] = $dependant;
+				}
+			}
+		}
+	}
+
+}
---
+++ StyleManager.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Mishak\WebResourceManagement;
+
+use Nette\Utils\Html;
+
+class StyleManager {
+
+	/**
+	 * Definition of styles
+	 *
+	 * @var array
+	 */
+	private $styles = [];
+
+	public function __construct($styles)
+	{
+		$this->styles = $styles;
+	}
+
+	public function setRequired($styles)
+	{
+		$this->required = [];
+		$this->dependencies = [];
+		$this->queue = [];
+		foreach ($styles as $style) {
+			$this->add($style);
+		}
+		return $this;
+	}
+
+	private $path;
+
+	/**
+	 * Set server path to styles
+	 *
+	 * @param string $path
+	 */
+	public function setPath($path)
+	{
+		$this->path = $path;
+		return $this;
+	}
+
+	private $presenter;
+
+	/**
+	 * Set presenter (is passed to style config)
+	 *
+	 * @param string $presenter
+	 */
+	public function setPresenter($presenter)
+	{
+		$this->presenter = $presenter;
+		return $this;
+	}
+
+	public function output()
+	{
+		$fragment = Html::el();
+		while ($this->queue) {
+			$printed = FALSE;
+			$styles = $this->queue;
+			$this->queue = [];
+			foreach ($styles as $style) {
+				$fragment[] = $this->outputStyle($style);
+				$fragment[] = "\n";
+				$this->addStyleDependenciesToQueue($style);
+			}
+		}
+		return $fragment;
+	}
+
+	private $minified = TRUE;
+
+	/**
+	 * All required styles
+	 *
+	 * @var array
+	 */
+	private $required;
+
+	/**
+	 * Map of style dependencies
+	 *
+	 * @var array[$styleName][$dependantName] = $dependant
+	 */
+	private $dependencies;
+
+	/**
+	 * Queue of styles to print
+	 *
+	 * @var array
+	 */
+	private $queue;
+
+	/**
+	 * Adds style identified by name to required styles
+	 *
+	 * @param string $name
+	 * @return object
+	 */
+	private function add($name)
+	{
+		if (isset($this->required[$name])) {
+			return $this->required[$name];
+		}
+		if (!isset($this->styles[$name])) {
+			throw new \Exception("Style '$name' has no definition.");
+		}
+		$style = (object) $this->styles[$name];
+		$style->name = $name;
+		$style->printed = FALSE;
+		$style->depends = isset($style->depends) ? (is_array($style->depends) ? $style->depends : [ $style->depends ]) : [];
+		foreach ($style->depends as $dependency) {
+			$this->add($dependency);
+			$this->dependencies[$dependency][$style->name] = $style;
+		}
+		if (!$style->depends) {
+			$this->queue[] = $style;
+		}
+		return $this->required[$name] = $style;
+	}
+
+	private function outputStyle($style)
+	{
+		$fragment = Html::el();
+		if (!empty($style->include)) {
+			$fragment->create('style', ['type' => 'text/css'])->setText(file_get_contents(WWW_DIR . '/js/' . $filename));
+			$style->printed = TRUE;
+			return $fragment;
+		}
+		$minified = $this->presenter->context->parameters['productionMode'];
+		$md5 = md5_file($style->filename);
+		$dir = WWW_DIR . '/generated/styles';
+		$extension = '.css';
+		if ($minified) {
+			$extension = '.min' . $extension;
+		}
+		$output = $dir . '/' . $md5 . $extension;
+		if (!file_exists($output)) {
+			if (!is_dir($dir)) {
+				mkdir($dir, 0755, TRUE);
+			}
+			if (!file_exists($style->filename)) {
+				throw new \Exception("Missing style '$style->name' file '$style->filename'.");
+			}
+			$command = [ 'lessc', escapeshellarg($style->filename) ];
+			if ($minified) {
+				$command[] = '-compress';
+			}
+			$contents = shell_exec(implode(' ', $command));
+			$contents = preg_replace_callback('/url\(([^)]+)\)/i', function ($matches) use ($style, $output, $md5, $extension) {
+				$url = trim($matches[1], '\'"');
+				$dir = substr($output, 0, -strlen($extension));
+				$resource = $url;
+				while ('../' == substr($resource, 0, 3)) {
+					$resource = substr($resource, 3);
+				}
+				if (!is_dir($dir . '/' . dirname($resource))) {
+					mkdir($dir . '/' . dirname($resource), 0755, TRUE);
+				}
+				copy(dirname($style->filename) . '/' . $url, $dir . '/' . $resource);
+				return 'url(' . $md5 . '/' . $resource . ')';
+			}, $contents);
+			file_put_contents($output, $contents);
+			file_put_contents($output . '.gz', gzencode($contents));
+			touch($output . '.gz', filemtime($output));
+		}
+		$fragment->create('link', [
+			'href' => $this->path . '/generated/styles/' . $md5 . $extension,
+			'rel' => 'stylesheet',
+			'type' => 'text/css'
+		]);
+		$style->printed = TRUE;
+		return $fragment;
+	}
+
+	private function addStyleDependenciesToQueue($style)
+	{
+		if (isset($this->dependencies[$style->name])) {
+			foreach ($this->dependencies[$style->name] as $dependant) {
+				if ($dependant->printed) {
+					continue;
+				}
+				$pendingDependencies = FALSE;
+				foreach ($dependant->depends as $name) {
+					if (!$this->required[$name]->printed) {
+						$pendingDependencies = TRUE;
+						break;
+					}
+				}
+				if (!$pendingDependencies) {
+					$this->queue[] = $dependant;
+				}
+			}
+		}
+	}
+
+}
